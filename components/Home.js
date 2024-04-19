6ee509ffc148634781c1323e531cf63fab8967de
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2024-04-19 18:36:17 +0300

    added text to homepage

--- components/Home.js
+++ components/Home.js
@@ -3,7 +3,15 @@ import { Text, StyleSheet, View } from "react-native"
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            <Text style={styles.header}>The App For Your Workout</Text>
+            <Text style={styles.header}>Needs</Text>
+            <Text style={styles.text}>In the "New Workout" page you can</Text>
+            <Text style={{color: "white", fontSize: 25}}>search for exercises by:</Text>
+            <Text style={{color: "white", fontSize: 20}}>* Exercise name</Text>
+            <Text style={{color: "white", fontSize: 20}}>* Bodypart</Text>
+            <Text style={{color: "white", fontSize: 20}}>* Equipment</Text>
+            <Text style={styles.text}>In the "My Workouts" you can view</Text>
+            <Text style={{color: "white", fontSize: 25}}>your saved workouts</Text>
         </View>
     )
 }
@@ -15,4 +23,13 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    text: {
+        color: "white",
+        marginTop: 50,
+        fontSize: 25
+    },
+    header: {
+        color: "white",
+        fontSize: 30
+    }
   });
